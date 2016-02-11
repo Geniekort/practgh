a0a0f2eeccd7ae7feac80a1c45d77ba902d8237a
GerritKaasjager <gerritkaasjager@solcon.nl>
2016-02-11 14:40:45 +0100

    Mywork

---
+++ src/practgh/Functions.java
@@ -0,0 +1,26 @@
+package practgh;
+
+import java.util.Scanner;
+
+public class Functions  {
+
+	public int X,Y;
+	
+	static int[][][]run(){
+		int[][][] output;
+		Scanner in = new Scanner(new Filereader());
+		int x = in.nextInt();
+		int y = in.nextInt();
+		
+		String[] X = new String[x];
+		
+		for(int i = 0;  i < x; i++){
+			X[i] = in.next();
+		}
+		in.close();
+		
+		
+			
+		}
+	}
+
--- src/practgh/Painter.java
+++ src/practgh/Painter.java
@@ -1,5 +1,7 @@
 package practgh;
 
 public class Painter {
-
+	
+	
 }
+
